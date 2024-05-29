90abe142d1bcb0968f8e80b4dfed7c0e7add821c
Abdulftah <abdulfta78@hotmail.com>
2024-05-29 20:37:32 +0300

    first commit

---
+++ scripts/projects.js
@@ -0,0 +1,27 @@
+
+var PROJECTS = [
+    {
+    img: "proj_1.jpeg",
+    titel: "project-1",
+    description: "about project-1"
+     
+    },
+
+    {
+        img: "profile_img.jpeg",
+        titel: "project-2",
+        description: "about project-2"
+         
+    },
+    {
+    img: "proj_1.jpeg",
+    titel: "project-3",
+    description: "about project-3"
+     
+    },
+]
+    
+
+
+
+
